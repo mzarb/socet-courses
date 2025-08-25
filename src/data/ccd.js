89983628876b9391ffea6272a8ad0fd2ea10ca04
m.zarb <m.zarb@rgu.ac.uk>
2025-08-25 14:20:54 +0100

    route attempt 9

--- src/data/ccd.js
+++ src/data/ccd.js
@@ -133,8 +133,8 @@ const ccdData =
                             type: "Route Choice",
                             size: 1, source: "subject",
                             options: [
-                                "Route A - " + electives.stage3.semester2.ccdElec,
-                                "Route B - " + electives.stage3.semester2.gdElec,
+                                moduleList.CM3149,
+                                moduleList.CM3150,
                             ],
                         },
                         {
