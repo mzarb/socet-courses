ba5ecea32749d4151d44fb77356c02998c2e3836
m.zarb <m.zarb@rgu.ac.uk>
2026-03-03 15:23:12 +0000

    fix DSAI and DSBM

--- src/data/dsai.js
+++ src/data/dsai.js
@@ -1,5 +1,5 @@
 import { coreModules } from './coremodules';
-import { electives } from './electives';
+//import { electives } from './electives';
 import { moduleList } from './listofmodules';
 
 const dsaiData = 
